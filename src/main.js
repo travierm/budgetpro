02792e01055d21b193462f62b1c53d7578ca4f0a
Travier Moorlag <moorlagt@gmail.com>
2024-10-03 20:56:14 -0400

    tabs are working

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import './app.css'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, onBeforeRouteUpdate } from 'vue-router'
 import MainPage from './pages/MainPage.vue'
 import AboutPage from './pages/AboutPage.vue'
 import MemoryPage from './pages/MemoryPage.vue'
 import './lib/appData'
+import { useReactiveState } from './lib/localStorage'
 
 const routes = [
     { path: '/', component: MainPage },
+    { path: '/:index', component: MainPage },
     { path: '/memory', component: MemoryPage },
     { path: '/about', component: AboutPage },
 ]
@@ -16,6 +18,19 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+const currentRevision = useReactiveState('CURRENT_REVISION', 0)
+router.beforeEach((to, from, next) => {
+    if (to.path === '/') {
+        currentRevision.value = 0
+    }
+
+    if (to.params.index !== undefined) {
+        currentRevision.value = to.params.index
+    }
+
+    next()
 })
 
 const app = createApp(App).use(router).mount('#app')
