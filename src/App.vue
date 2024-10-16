792f7af6c539ddb933273ca0776345c97e63d5e2
Travier Moorlag <moorlagt@gmail.com>
2024-10-16 10:34:23 -0400

    kinda working

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,45 @@
 <script setup>
-import { computed } from 'vue';
-import MainPage from './pages/MainPage.vue';
 import Navbar from './components/Navbar.vue';
 import { RouterView } from 'vue-router';
+import { BanknotesIcon } from '@heroicons/vue/24/outline';
 </script>
 
 <template>
   <div class="lg:mx-[150px] mx-[20px] md:mx-[40px]">
-    <h1 class="text-4xl font-bold text-center my-4 gradient-title">BudgetPro</h1>
+
+    <div class="flex justify-center items-center gradient-wrapper">
+      <BanknotesIcon class="w-8 h-8 mr-2 gradient-icon" />
+      <h1 class="text-4xl font-bold text-center my-4 gradient-text">BudgetPro</h1>
+    </div>
     <div>
       <Navbar />
       <RouterView />
     </div>
   </div>
+
+  <svg width="0" height="0" aria-hidden="true" style="position: absolute;">
+    <defs>
+      <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" stop-color="#1e90ff" />
+        <stop offset="100%" stop-color="#32cd32" />
+      </linearGradient>
+    </defs>
+  </svg>
 </template>
 
 <style scoped>
-.gradient-title {
-  background: linear-gradient(to right, #64748b, #94a3b8);
+.gradient-text {
+  background: linear-gradient(to right, #1e90ff, #32cd32);
   -webkit-background-clip: text;
   background-clip: text;
   color: transparent;
+}
+
+.gradient-icon {
+  color: transparent;
+}
+
+.gradient-icon :deep(path) {
+  stroke: url(#gradient);
 }
 </style>
